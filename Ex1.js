48b1142977abda46f5f389c4f96df297d1078085
raphaelcavalcanti <raphaelcavalcanti89@gmail.com>
2023-02-16 23:44:39 -0300

    aulas1602

---
+++ Ex1.js
@@ -0,0 +1,12 @@
+const nome = "Lara"
+const idade = 2
+const sobrenome = "Ayumi"
+const bacharel = false
+const dezAnosDepois = "12"
+
+const nomeCompleto = nome + sobrenome
+const resultado = idade + dezAnosDepois
+
+console.log(nomeCompleto)
+console.log(resultado)
+console.log(bacharel == 1)
---
+++ tipos.js
@@ -0,0 +1,7 @@
+let input = "teste";
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
